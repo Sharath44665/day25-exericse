8e132fb4fc47e95f7a4095b59526dfe708c0f9d1
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-18 15:12:54 +0530

    add pandas module and print some stuff

--- main.py
+++ main.py
@@ -4,15 +4,20 @@
 
 import csv
 
-with open("weather_data.csv") as myfile:
-    w_data=csv.reader(myfile)
-    # print(w_data)
-    temperature_data=[]
-    count=0
-    for row in w_data:
-        if count != 0:
-            temp=row[1]
-            temperature_data.append(int(temp))
-        count+=1
+# with open("weather_data.csv") as myfile:
+#     w_data=csv.reader(myfile)
+#     # print(w_data)
+#     temperature_data=[]
+#     count=0
+#     for row in w_data:
+#         if count != 0:
+#             temp=row[1]
+#             temperature_data.append(int(temp))
+#         count+=1
+#
+#     print(temperature_data)
 
-    print(temperature_data)
+import pandas
+w_data= pandas.read_csv("weather_data.csv")
+print(w_data)
+print(w_data["temp"])
