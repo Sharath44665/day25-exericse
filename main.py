ef901608da8f4eea97b006b8333540916989c942
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-18 17:04:42 +0530

    done with day25

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import csv
 
 import pandas
 
-w_data = pandas.read_csv("weather_data.csv")
+# w_data = pandas.read_csv("weather_data.csv")
 # # print(w_data)
 # # print(w_data["temp"])
 #
@@ -34,7 +34,7 @@ w_data = pandas.read_csv("weather_data.csv")
 # max_temp=w_data["temp"].max()
 # print(max_temp)
 
-monday_row = w_data[w_data["day"] == "Monday"]
+# monday_row = w_data[w_data["day"] == "Monday"]
 # print(monday_row)
 #
 # high_temperature=w_data[w_data["temp"]==w_data["temp"].max()]
@@ -46,11 +46,28 @@ monday_row = w_data[w_data["day"] == "Monday"]
 # print(far_temp)
 
 # create a dataframe from scractch
-data_dict={
-    "students": ["name1","name2","name3",],
-    "score":[95,65,75]
+# data_dict={
+#     "students": ["name1","name2","name3",],
+#     "score":[95,65,75]
+# }
+#
+# actual_data=pandas.DataFrame(data_dict)
+# print(actual_data)
+# actual_data.to_csv("student_data.csv")
+# download from here:https://data.cityofnewyork.us/Environment/2018-Central-Park-Squirrel-Census-Squirrel-Data/vfnx-vebw
+squirrel_data=pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrel=squirrel_data[squirrel_data["Primary Fur Color"]=="Gray"]
+grey_squirrel=len(grey_squirrel)
+black_color=squirrel_data[squirrel_data["Primary Fur Color"]=="Black"]
+black_color=len(black_color)
+red = squirrel_data[squirrel_data["Primary Fur Color"]=="Cinnamon"]
+red= len(red)
+
+my_data={
+    "grey squirrel": [grey_squirrel],
+    "black squirrel": [black_color],
+    "Red squirrel": [red]
 }
 
-actual_data=pandas.DataFrame(data_dict)
-print(actual_data)
-actual_data.to_csv("student_data.csv")
+my_analytics_data=pandas.DataFrame(my_data)
+my_analytics_data.to_csv("my_analytics.csv")
