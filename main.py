bb3d2de4e1520254b3266282979b7db667c7eb6c
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-18 16:12:03 +0530

    work wigh basics pandas framework

--- main.py
+++ main.py
@@ -18,6 +18,39 @@ import csv
 #     print(temperature_data)
 
 import pandas
-w_data= pandas.read_csv("weather_data.csv")
-print(w_data)
-print(w_data["temp"])
+
+w_data = pandas.read_csv("weather_data.csv")
+# # print(w_data)
+# # print(w_data["temp"])
+#
+# w_dict=w_data.to_dict()
+# print(w_dict)
+# w_list=w_data["temp"].to_list()
+# # avg_temp=sum(w_list) / len(w_list)
+# # print(avg_temp)
+# # _______________________or
+# avg_temp=w_data["temp"].mean()
+# print(avg_temp)
+# max_temp=w_data["temp"].max()
+# print(max_temp)
+
+monday_row = w_data[w_data["day"] == "Monday"]
+# print(monday_row)
+#
+# high_temperature=w_data[w_data["temp"]==w_data["temp"].max()]
+# print(high_temperature)
+
+# w_data[w_data["day"]=="Monday"]
+# actual_temp=int(monday_row["temp"])
+# far_temp=actual_temp *9/5 +32
+# print(far_temp)
+
+# create a dataframe from scractch
+data_dict={
+    "students": ["name1","name2","name3",],
+    "score":[95,65,75]
+}
+
+actual_data=pandas.DataFrame(data_dict)
+print(actual_data)
+actual_data.to_csv("student_data.csv")
