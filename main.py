37fcc85c85b1fe074b2f2c546727e220a6568946
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-18 15:05:26 +0530

    add temperatures to list as integer

---
+++ main.py
@@ -0,0 +1,18 @@
+# with open("weather_data.csv") as myfile:
+#     w_data=myfile.readlines()
+#     print(w_data)
+
+import csv
+
+with open("weather_data.csv") as myfile:
+    w_data=csv.reader(myfile)
+    # print(w_data)
+    temperature_data=[]
+    count=0
+    for row in w_data:
+        if count != 0:
+            temp=row[1]
+            temperature_data.append(int(temp))
+        count+=1
+
+    print(temperature_data)
